70520d5478f227685c3f30165e342e57e0293fbc
erecem <40902954+erecem@users.noreply.github.com>
2018-10-12 11:36:08 -0300

    Update ClinicaVeterinaria.java
    cambios minimos

--- clinicaveterinaria-master/src/clinicaveterinaria/modelo/ClinicaVeterinaria.java
+++ clinicaveterinaria-master/src/clinicaveterinaria/modelo/ClinicaVeterinaria.java
@@ -21,51 +21,51 @@ public class ClinicaVeterinaria {
      */
     public static void main(String[] args) {
        
-        Conexion conexion = null;
-     
-          
+     Conexion conexion = null;
+                
+                
+                
+                try {
+                    conexion = new Conexion("jdbc:mysql://localhost/clinica_veterinaria","root","");
+                
+                
+               // Cliente clientes = new Cliente("moreno christian","3584284451","mitre 1224","aishi gimenez",35475534);
+               Cliente cliente2 = new Cliente("luciano rosa","3585534","caseros 1000","alvaro rosa",35455734);
+              Mascota mascota = new Mascota("perrito","mezcla","marron","macho","wincop",LocalDate.of(2003, 2, 15),0001,cliente2);
+               // Mascota mascota2 = new Mascota("perro","mezcla","marron","macho","winquiy",LocalDate.of(2003, 2, 15),0001,clientes);
+               // Mascota mascota3 = new Mascota("perro","mezcla","marron","macho","winc",LocalDate.of(2003, 2, 15),0001,clientes);
+              ClienteData clientedata = new ClienteData(conexion);
+                 clientedata.guardarCliente(cliente2);
+                 
         
-            try {
-                conexion = new Conexion("jdbc:mysql://localhost/laveterinaria","root", "");
-            } catch (ClassNotFoundException ex) {
-                Logger.getLogger(ClinicaVeterinaria.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        
-        Cliente clientes = new Cliente("moreno christian","3584284451","mitre 1224","aishi gimenez",35475534);
-        Cliente cliente2 = new Cliente("luciano rosa","3585534","caseros 1000","alvaro rosa",35455734);
-        Mascota mascota = new Mascota("perro","mezcla","marron","macho","winca",LocalDate.of(2003, 2, 15),0001,clientes);
-        
-        
-        
-        ClienteData clientedata = new ClienteData(conexion);
-        //clientedata.guardarCliente(clientes);
-        MascotaData mascotadata = null;
-        try {
-            mascotadata = new MascotaData (conexion);
-        } catch (SQLException ex) {
+            MascotaData mascotadata = new MascotaData (conexion);
+            mascotadata.guardarMascota(mascota);
+            // clientes.agregarMascota(mascota);
+            // clientes.agregarMascota(mascota2);
+            // clientes.agregarMascota(mascota3);
+            // mascotadata.guardarMascota(mascota);
+            // clientes.clienteMascotas();
+            //clientedata.borrarCliente(1);
+            
+            //  System.out.println("el cliente de id 2 es: "+clientedata.buscarCliente(2).getNombreApellido());
+            /*
+            ArrayList <Cliente> lista ;
+            ArrayList <Mascota> lista2 ;
+            lista2 =  (ArrayList<Mascota>) mascotadata.obtenerMascota();
+            lista = (ArrayList<Cliente>) clientedata.obtenerCliente();
+            
+            
+            // clientedata.obtenerCliente().forEach(cliente -> {
+            //     System.out.println("Nombre: " + cliente.getNombreApellido() );
+            //  });
+            
+            */
+      
+     } catch (ClassNotFoundException ex) {
+                    Logger.getLogger(ClinicaVeterinaria.class.getName()).log(Level.SEVERE, null, ex);
+               
             Logger.getLogger(ClinicaVeterinaria.class.getName()).log(Level.SEVERE, null, ex);
         }
-        mascotadata.guardarMascota(mascota);
-        
-        
-      
-      
-      
-      /*ClienteData clientedata = new ClienteData(conexion);
-      clientedata.obtenerCliente().forEach(clientes -> {
-                System.out.println("Nombre: " + cliente.getNombreApellido() );
-            });
-     */
-     } 
-     //catch (ClassNotFoundException ex) {
-       //  Logger.getLogger(ClinicaVeterinaria.class.getName()).log(Level.SEVERE, null, ex); 
-     //catch (ClassNotFoundException ex) {
-       //  Logger.getLogger(ClinicaVeterinaria.class.getName()).log(Level.SEVERE, null, ex);
-     }
-       
-         
-     
-  //  }
   
-  
-//}
+  }
+}
