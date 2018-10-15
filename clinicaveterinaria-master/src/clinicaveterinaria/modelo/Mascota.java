192ddfda2415e15c810c686b5c1d574cbe65a828
erecem <40902954+erecem@users.noreply.github.com>
2018-10-15 11:30:21 -0300

    Update Mascota.java
    ..

--- clinicaveterinaria-master/src/clinicaveterinaria/modelo/Mascota.java
+++ clinicaveterinaria-master/src/clinicaveterinaria/modelo/Mascota.java
@@ -34,7 +34,7 @@ public class Mascota {
     }
 
     Mascota() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
  
 
